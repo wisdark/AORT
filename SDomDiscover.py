6abb5f39ef00fbccba6e5a5c62bea2256292de9f
D3Ext <67125397+D3Ext@users.noreply.github.com>
2022-07-20 12:38:24 +0200

    Add files via upload

--- SDomDiscover.py
+++ SDomDiscover.py
@@ -4,12 +4,14 @@
 import signal
 import requests
 import sys
-import re, json
+import re
+import json
 import argparse
 import dns.zone
 import dns.resolver
 import pydig
 from time import sleep
+import pdb
 
 # Output Colours
 class c:
@@ -55,8 +57,10 @@ def parseArgs():
     p.add_argument("-d", "--domain", help="domain to search its subdomains", required=True)
     p.add_argument("-a", "--axfr", help="try a domain zone transfer attack", action='store_true', required=False)
     p.add_argument("-m", "--mail", help="try to enumerate mail servers", action='store_true', required=False)
+    p.add_argument('-e', '--extra', help="look for extra dns information", action='store_true', required=False)
     p.add_argument("-n", "--nameservers", help="try to enumerate the name servers", action='store_true', required=False)
     p.add_argument("-i", "--ip", help="it reports the ip or ips of the domain", action='store_true', required=False)
+    p.add_argument('-6', '--ipv6', help="enumerate the ipv6 of the domain", action='store_true', required=False)
     p.add_argument("-o", "--output", help="file to store the scan output", required=False)
     p.add_argument("--all", help="perform all the enumeration at once", action='store_true', required=False)
     p.add_argument("--version", help="display the script version", action='store_true', required=False)
@@ -66,39 +70,71 @@ def parseArgs():
 # Nameservers Function 
 def ns_enum(domain):
     print(c.BLUE + "\n[" + c.END + c.GREEN + "+" + c.END + c.BLUE + "] Trying to discover valid name servers...\n" + c.END)
-    sleep(0.5)
+    sleep(0.2)
     data = pydig.query(domain, 'NS')
 
-    for ns in data:
-        l = len(ns)
-        ns = ns[:l-1]
-        print(c.YELLOW + ns + c.END)
+    if data:
+        for ns in data:
+            l = len(ns)
+            ns = ns[:l-1]
+            print(c.YELLOW + ns + c.END)
+    else:
+        print(c.YELLOW + "Unable to enumerate" + c.END)
 
 # IPs discover Function
 def ip_enum(domain):
-    print(c.BLUE + "\n[" + c.END + c.GREEN + "+" + c.END + c.BLUE + "] Trying to discover the IPs...\n" + c.END)
-    sleep(0.5)
+    print(c.BLUE + "\n[" + c.END + c.GREEN + "+" + c.END + c.BLUE + "] Discovering IPs of the domain...\n" + c.END)
+    sleep(0.2)
     data = pydig.query(domain, 'A')
 
-    for ip in data:
-        print(c.YELLOW + ip + c.END)
+    if data:
+        for ip in data:
+            print(c.YELLOW + ip + c.END)
+    else:
+        print(c.YELLOW + "Unable to enumerate" + c.END)
+
+# Extra DNS info Function
+def txt_enum(domain):
+    print(c.BLUE + "\n[" + c.END + c.GREEN + "+" + c.END + c.BLUE + "] Enumerating extra DNS information...\n" + c.END)
+    sleep(0.2)
+    data = pydig.query(domain, 'TXT')
+
+    if data:
+        for info in data:
+            print(c.YELLOW + info + c.END)
+    else:
+        print(c.YELLOW + "Unable to enumerate" + c.END)
+
+def ipv6_enum(domain):
+    print(c.BLUE + "\n[" + c.END + c.GREEN + "+" + c.END + c.BLUE + "] Getting ipv6 of the domain...\n" + c.END)
+    sleep(0.2)
+    data = pydig.query(domain, 'AAAA')
+    
+    if data:
+        for info in data:
+            print(c.YELLOW + info + c.END)
+    else:
+        print(c.YELLOW + "Unable to enumerate" + c.END)
 
 # Mail servers Function
 def mail_enum(domain):
-    print(c.BLUE + "\n[" + c.END + c.GREEN + "+" + c.END + c.BLUE + "] Trying to discover valid mail servers...\n" + c.END)
-    sleep(0.5)
+    print(c.BLUE + "\n[" + c.END + c.GREEN + "+" + c.END + c.BLUE + "] Finding valid mail servers...\n" + c.END)
+    sleep(0.2)
     data = pydig.query(domain, 'MX')
 
-    for mail_output in data:
-        mail_output = mail_output.split(' ')[1]
-        l = len(mail_output)
-        mail_servers = mail_output[:l-1]
-        print(c.YELLOW + mail_servers + c.END)
+    if data:
+        for mail_output in data:
+            mail_output = mail_output.split(' ')[1]
+            l = len(mail_output)
+            mail_servers = mail_output[:l-1]
+            print(c.YELLOW + mail_servers + c.END)
+    else:
+        print(c.YELLOW + "Unable to enumerate" + c.END)
 
 # Domain Zone Transfer Attack Function
 def axfr(domain):
-    print(c.BLUE + "\n[" + c.END + c.GREEN + "*" + c.END + c.BLUE + "] Starting Domain Zone Transfer attack...\n" + c.END)
-    sleep(0.5)
+    print(c.BLUE + "\n[" + c.END + c.GREEN + "+" + c.END + c.BLUE + "] Starting Domain Zone Transfer attack...\n" + c.END)
+    sleep(0.2)
     ns_answer = dns.resolver.resolve(domain, 'NS')
     for server in ns_answer:
         print(c.YELLOW + "[*] Found NS: {}".format(server) + c.END)
@@ -117,13 +153,18 @@ def axfr(domain):
 def SDom(domain,filename):
     banner()
     print(c.BLUE + "\n[" + c.END + c.GREEN + "+" + c.END + c.BLUE + "] Discovering valid subdomains...\n" + c.END)
-    sleep(0.5)
+    sleep(0.1)
 
     r = requests.get("https://crt.sh/?q=" + domain + "&output=json", timeout=20)
     formatted_json = json.dumps(json.loads(r.text), indent=4)
     domains = re.findall(r'"common_name": "(.*?)"', formatted_json)
 
-    doms = sorted(set(domains))
+    doms = []
+
+    raw_doms = sorted(set(domains))
+    for dom in raw_doms:
+        if dom.endswith(domain):
+            doms.append(dom)
 
     if filename != None:
         f = open(filename, "a")
@@ -158,6 +199,7 @@ def SDom(domain,filename):
                     f.write(value + "\n")
 
     print("+" + "-"*39 + "+" + c.END)
+    print(c.YELLOW + "\nTotal discovered sudomains: " + str(len(doms) - 1) + c.END)
 
     if filename != None:
         f.close()
@@ -166,13 +208,13 @@ def SDom(domain,filename):
 # Program workflow starts here
 if __name__ == '__main__':
 
-    parse = parseArgs()
-
     # If --version is passed
-    if parse.version:
+    if "--version" in sys.argv:
         print("\nSDomDiscover v1.0 - By D3Ext")
         print("Contact me: <d3ext@proton.me>\n")
         sys.exit(0)
+
+    parse = parseArgs()
 
     # If --output is passed
     if parse.output:
@@ -191,14 +233,12 @@ if __name__ == '__main__':
             domain = domain.split('http://')[1]
 
         SDom(domain,filename)
-
         axfr(domain)
-
         mail_enum(domain)
-
         ns_enum(domain)
-
         ip_enum(domain)
+        ipv6_enum(domain)
+        txt_enum(domain)
 
         sys.exit(0)
 
@@ -223,6 +263,12 @@ if __name__ == '__main__':
 
         if parse.ip:
             ip_enum(domain)
+
+        if parse.ipv6:
+            ipv6_enum(domain)
+
+        if parse.extra:
+            txt_enum(domain)
 
         sys.exit(0)
 
